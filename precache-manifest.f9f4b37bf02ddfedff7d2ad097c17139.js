cc4dc67b1e5abaacefe9ae6aa19aa0b5519a8546
jason tlh <jasonjdh94@gmail.com>
2020-10-27 15:06:13 -0400

    Updates

---
+++ precache-manifest.f9f4b37bf02ddfedff7d2ad097c17139.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e0cb2b6b8699d9caf96e8c9b6d6cf4",
+    "url": "/SmartQ-app/index.html"
+  },
+  {
+    "revision": "8bcd65e49106481a8f09",
+    "url": "/SmartQ-app/static/css/main.293c13d4.chunk.css"
+  },
+  {
+    "revision": "c84ac1b6f812a450e17e",
+    "url": "/SmartQ-app/static/js/2.d5af77a0.chunk.js"
+  },
+  {
+    "revision": "22c95ed68071a711843b9981e2bca1af",
+    "url": "/SmartQ-app/static/js/2.d5af77a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcd65e49106481a8f09",
+    "url": "/SmartQ-app/static/js/main.6329ebf3.chunk.js"
+  },
+  {
+    "revision": "e4de8f56c33b45a212ee",
+    "url": "/SmartQ-app/static/js/runtime-main.d923a8bf.js"
+  },
+  {
+    "revision": "4959953041ab4d498855d302c5213b8c",
+    "url": "/SmartQ-app/static/media/destinations.49599530.svg"
+  },
+  {
+    "revision": "5a96cdb79bc37bb64a7e77fa9f2f0d0b",
+    "url": "/SmartQ-app/static/media/phone.5a96cdb7.svg"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/SmartQ-app/static/media/profile_pic.5a021ab9.png"
+  },
+  {
+    "revision": "5441cb6e4d028f97d2b22ee1a34c64ab",
+    "url": "/SmartQ-app/static/media/queue.5441cb6e.svg"
+  },
+  {
+    "revision": "ebb30b9ec9cf1661fd5939da30b5a69e",
+    "url": "/SmartQ-app/static/media/shopping.ebb30b9e.svg"
+  },
+  {
+    "revision": "f3754ca476c39bf2d454bc5c7948e435",
+    "url": "/SmartQ-app/static/media/wheelchair.f3754ca4.svg"
+  }
+]);
